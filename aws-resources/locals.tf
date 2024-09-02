dac8e643d1fd6b1911256cdec3a9cc35b39ddb98
josembar <jose.barrantes@hotmail.es>
2024-09-02 11:56:33 -0500

    feat: include locals

---
+++ aws-resources/locals.tf
@@ -0,0 +1,18 @@
+locals {
+  app_prefix = "circuit-breaker-demo"
+  app_port   = 80
+  app_path   = "hello"
+  image_tag  = "latest"
+  ecr_definition = {
+    "${local.app_prefix}-node" = {
+      image_tag_mutability = "MUTABLE"
+      force_delete         = true
+      scan_on_push         = false
+    },
+    "${local.app_prefix}-java" = {
+      image_tag_mutability = "MUTABLE"
+      force_delete         = true
+      scan_on_push         = false
+    }
+  }
+}
