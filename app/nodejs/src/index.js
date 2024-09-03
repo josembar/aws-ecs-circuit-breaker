ac135f284013cfee230ae7dd86e9e9a54dba162f
josembar <jose.barrantes@hotmail.es>
2024-09-03 11:31:17 -0500

    refactor: changed to port 3000

--- app/nodejs/src/index.js
+++ app/nodejs/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 80
+const port = process.env.PORT || 3000
 
 const app = express();
 
