10ec7e7190cc498e45cdd6f95bb0ee86e6144fc5
josembar <jose.barrantes@hotmail.es>
2024-09-03 11:32:14 -0500

    feat: include / endpoint

--- app/nodejs/src/routes/appRoutes.js
+++ app/nodejs/src/routes/appRoutes.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    res.sendStatus(200);
+    res.json(
+        {
+            "message": "ok"
+        }
+    );
+});
+
 router.get('/hello', async (req, res) => {
     res.sendStatus(200);
     res.json(
