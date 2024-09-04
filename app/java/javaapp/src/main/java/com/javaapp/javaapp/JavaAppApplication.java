109d80af77f0dbe4839d73ab6cabe5e51380d0b9
josembar <jose.barrantes@hotmail.es>
2024-09-04 14:59:56 -0500

    refactor: renamed main class

--- app/java/javaapp/src/main/java/com/javaapp/javaapp/JavaAppApplication.java
+++ app/java/javaapp/src/main/java/com/javaapp/javaapp/JavaAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaappApplication {
+public class JavaAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaappApplication.class, args);
+		SpringApplication.run(JavaAppApplication.class, args);
 	}
 
 }
