d9bcc0672b6250f27e0deb2f1a8099dab3870360
josembar <jose.barrantes@hotmail.es>
2024-09-04 14:44:21 -0500

    feat: added rest controller

---
+++ app/java/javaapp/src/main/java/com/javaapp/javaapp/Controller.java
@@ -0,0 +1,25 @@
+package com.javaapp.javaapp;
+
+import java.util.Collections;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping(path = "/", produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> getRoot() {
+    	return ResponseEntity.status(HttpStatus.OK).body(
+            	Collections.singletonMap("message", "ok"));
+	}
+
+	@GetMapping(path = "/hello", produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> getHello() {
+    	return ResponseEntity.status(HttpStatus.OK).body(
+            	Collections.singletonMap("message", "hello"));
+	}
+}
