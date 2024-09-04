4c8c9c39387e2ac9505169ef97479d565050d1e1
josembar <jose.barrantes@hotmail.es>
2024-09-04 15:50:49 -0500

    feat: new method to validate if environment variable for port was set

--- app/java/javaapp/src/main/java/com/javaapp/javaapp/Server.java
+++ app/java/javaapp/src/main/java/com/javaapp/javaapp/Server.java
@@ -3,12 +3,19 @@ package com.javaapp.javaapp;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.stereotype.Component;
+import org.apache.commons.lang3.ObjectUtils;
 
 @Component
 public class Server implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    static Integer getPort() {
+        String port = System.getenv("PORT");
+        return ObjectUtils.allNull(port) ? 3000 : Integer.parseInt(port);
+    }
+
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
           // Setting the port number
-        factory.setPort(3000);
+        factory.setPort(getPort());
     }
 }
