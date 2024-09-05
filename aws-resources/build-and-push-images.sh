72b9a7e5363ea6e4a6b7535c9f0df2fe88695fa0
josembar <jose.barrantes@hotmail.es>
2024-09-05 10:50:50 -0500

    feat: script to build images

---
+++ aws-resources/build-and-push-images.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+delete_image() {
+    image=$(docker images $1 -q)
+    if [ -z $image ]
+    then
+        echo "Image $1 not found"
+    else
+        docker image rm $image
+    fi
+}
+
+# AWS_REGION=$1
+# echo "AWS_REGION $AWS_REGION"
+
+# AWS_ACCOUNT_ID=$2
+# echo "AWS_ACCOUNT_ID $AWS_ACCOUNT_ID"
+
+# NODEJS_APP_IMAGE_NAME=$3
+# echo "NODEJS_APP_IMAGE_NAME $NODEJS_APP_IMAGE_NAME"
+
+# JAVA_APP_IMAGE_NAME=$4
+# echo "JAVA_APP_IMAGE_NAME $JAVA_APP_IMAGE_NAME"
+
+# NODEJS_ECR_REPO_NAME=$5
+# echo "ECR_REPO_NAME $ECR_REPO_NAME"
+
+# JAVA_ECR_REPO_NAME=$6
+# echo "ECR_REPO_NAME $ECR_REPO_NAME"
+
+# images=("$NODEJS_APP_IMAGE_NAME" "$JAVA_APP_IMAGE_NAME")
+images=("nodeapp" "javaapp")
+for image in ${images[@]}; do
+    delete_image $image
+done
+
+# cd ../
+
+# # # docker build -t $IMAGE_NAME ../app
+# docker build -t $DEMO_APP_IMAGE_NAME ../app
+
+# docker build -t $OTEL_IMAGE_NAME ./otel
+
+# docker build -t $THANOS_IMAGE_NAME ./thanos
+
+# aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+# # # docker tag $IMAGE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME
+# docker tag $DEMO_APP_IMAGE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO_NAME:$DEMO_APP_IMAGE_NAME-latest
+
+# docker tag $OTEL_IMAGE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO_NAME:$OTEL_IMAGE_NAME-latest
+
+# docker tag $THANOS_IMAGE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO_NAME:$THANOS_IMAGE_NAME-latest
+
+# # # docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME
+# docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO_NAME:$DEMO_APP_IMAGE_NAME-latest
+
+# docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO_NAME:$OTEL_IMAGE_NAME-latest
+
+# docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO_NAME:$THANOS_IMAGE_NAME-latest
