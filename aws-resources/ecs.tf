a6317fc9888d1131f64a0046f2cbb1636c8f7157
josembar <jose.barrantes@hotmail.es>
2024-09-06 12:26:31 -0500

    feat: ecs service definition

--- aws-resources/ecs.tf
+++ aws-resources/ecs.tf
@@ -54,22 +54,33 @@ resource "aws_ecs_task_definition" "this" {
   ]
 }
 
-# resource "aws_ecs_service" "this" {
-#   name            = "${local.app_prefix}-service"
-#   cluster         = aws_ecs_cluster.this.id
-#   task_definition = aws_ecs_task_definition.this.arn
-#   desired_count   = 1
-#   launch_type     = "FARGATE"
+resource "aws_ecs_service" "this" {
+  name            = "${local.app_prefix}-service"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.this.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
 
-#   load_balancer {
-#     target_group_arn = aws_alb_target_group.this.arn
-#     container_name   = local.demo_app_image_name
-#     container_port   = local.app_port
-#   }
+  deployment_controller {
+    type = "ECS"
+  }
 
-#   network_configuration {
-#     subnets          = data.aws_subnets.this.ids
-#     security_groups  = [aws_security_group.ecs.id]
-#     assign_public_ip = true
-#   }
-# }
+  deployment_circuit_breaker {
+    enabled  = true
+    rollback = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.this.arn
+    container_name   = local.app_prefix
+    container_port   = local.app_port
+  }
+
+  network_configuration {
+    subnets          = data.aws_subnets.this.ids
+    security_groups  = [aws_security_group.ecs.id]
+    assign_public_ip = true
+  }
+
+  depends_on = [aws_iam_policy.this]
+}
