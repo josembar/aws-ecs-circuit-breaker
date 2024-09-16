1b26d6225d4e77029f48d0b653ea12c0c5059b01
josembar <jose.barrantes@hotmail.es>
2024-09-16 08:56:05 -0500

    feat: include max and min healthy percentage

--- aws-resources/ecs.tf
+++ aws-resources/ecs.tf
@@ -58,11 +58,13 @@ resource "aws_ecs_task_definition" "this" {
 }
 
 resource "aws_ecs_service" "this" {
-  name            = "${local.app_prefix}-service"
-  cluster         = aws_ecs_cluster.this.id
-  task_definition = aws_ecs_task_definition.this.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
+  name                               = "${local.app_prefix}-service"
+  cluster                            = aws_ecs_cluster.this.id
+  task_definition                    = aws_ecs_task_definition.this.arn
+  desired_count                      = 1
+  launch_type                        = "FARGATE"
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 100
 
   deployment_controller {
     type = "ECS"
